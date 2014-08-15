91d68bc30b9e47448b94a3be7baf4f018c076679
Your Name <you@example.com>
2014-08-15 10:28:06 +0300

    add menu crud

---
+++ setup/protected/components/XMenu.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of XMenu
+ *
+ * @author mohamed
+ */
+class XMenu extends CApplicationComponent {
+
+    private static $menuTree = array();
+
+    public static function getMenuTree() {
+        if (empty(self::$menuTree)) {
+            $rows = Menu::model()->findAll('parent_id IS NULL');
+            foreach ($rows as $item) {
+                self::$menuTree[] = self::getMenuItems($item);
+            }
+        }
+        return self::$menuTree;
+    }
+
+    private static function getMenuItems($modelRow) {
+
+        if (!$modelRow)
+            return;
+
+        if (isset($modelRow->Childs)) {
+            $chump = self::getMenuItems($modelRow->Childs);
+            if ($chump != null)
+                $res = array('label' => $modelRow->title, 'items' => $chump, 'url' => Yii::app()->createUrl('yourcontroller/youraction', array('id' => $modelRow->id)));
+            else
+                $res = array('label' => $modelRow->title, 'url' => Yii::app()->createUrl('yourcontroller/youraction', array('id' => $modelRow->id)));
+            return $res;
+        } else {
+            if (is_array($modelRow)) {
+                $arr = array();
+                foreach ($modelRow as $leaves) {
+                    $arr[] = self::getMenuItems($leaves);
+                }
+                return $arr;
+            } else {
+                return array('label' => ($modelRow->title), 'url' => Yii::app()->createUrl('yourcontroller/youraction', array('id' => $modelRow->id)));
+            }
+        }
+    }
+
+}
